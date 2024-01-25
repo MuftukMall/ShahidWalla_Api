cd77daf6dafecc3cc721e5662915d18218361fdb
Muftuk Mall <155059661+MuftukMall@users.noreply.github.com>
2024-01-25 10:52:51 +0530

    Update batchModel.js

--- src/models/batchModel.js
+++ src/models/batchModel.js
@@ -4,7 +4,7 @@ const batchSchema = new Schema({
   name: { type: String, required: true },
   image: { type: String, required: true },
   slug: { type: String, unique: true },
-});
+}, { timestamps: { createdAt: true, updatedAt: false }});
 
 batchSchema.pre("save", async function (next) {
   const slug = this.name
