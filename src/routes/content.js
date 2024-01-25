c3bc6032c1b68993813d218641973c97adbdc1e1
Muftuk Mall <155059661+MuftukMall@users.noreply.github.com>
2024-01-25 10:44:36 +0530

    Update content.js

--- src/routes/content.js
+++ src/routes/content.js
@@ -10,7 +10,7 @@ content.get("/batch/:batchSlug/:subjectSlug/contents", async (req, res) => {
       .find({ subject: _slug })
       .find(req.query)
       .select(["-__v", "-subject", "-contentType"]).sort({ _id: 1 });
-    res.setHeader("Cache-Control", "public, s-maxage=60, stale-while-revalidate=60");
+    res.setHeader("Cache-Control", "public, s-maxage=1, stale-while-revalidate=1");
     res.send({ success: true, Data: GetContent });
   } catch (error) {
     res.status(400).send({ success: false, message: error.message });
