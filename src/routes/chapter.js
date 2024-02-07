437f1b07540ce0f0fd179f1a5fd9ae543c5c2766
Muftuk Mall <155059661+MuftukMall@users.noreply.github.com>
2024-02-07 12:28:58 +0530

    Update chapter.js

--- src/routes/chapter.js
+++ src/routes/chapter.js
@@ -6,7 +6,7 @@ const chapter = Router();
 chapter.get("/batch/:batchSlug/:subjectSlug", async (req, res) => {
   try {
     const _slug = req.params.subjectSlug;
-    const GetChapter = await chapterModel.find({ subject: _slug }).sort({ createdAt: 1 }); //select(["-__v", "-subject"])
+    const GetChapter = await chapterModel.find({}).sort({ createdAt: 1 }); //select(["-__v", "-subject"]).subject: _slug
     res.setHeader("Cache-Control", "public, s-maxage=240, stale-while-revalidate=300");
     res.send({ success: true, Data: GetChapter });
   } catch (error) {
